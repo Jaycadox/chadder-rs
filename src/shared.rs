7b2a10a27e566bd893768ffe7dc766e3fc85fce7
Jaycadox <68955944+Jaycadox@users.noreply.github.com>
2022-09-17 16:33:18 +1000

    Fix lua stuff (I hate git)
    Won't really be using branches for a little while.

--- src/shared.rs
+++ src/shared.rs
@@ -1,3 +1,6 @@
+use log::error;
+use mlua::prelude::LuaFunction;
+use mlua::{chunk, Lua};
 use rsa::{RsaPrivateKey, RsaPublicKey};
 use serde::{Deserialize, Serialize};
 use sodiumoxide::crypto::aead::xchacha20poly1305_ietf;
@@ -64,5 +67,102 @@ impl EncryptionInfo {
             nonce: xchacha20poly1305_ietf::gen_nonce(),
             key: xchacha20poly1305_ietf::gen_key(),
         }
+    }
+}
+pub struct LuaScript {
+    pub lua: Box<Lua>,
+    content: String,
+}
+
+unsafe impl Send for LuaScript {}
+
+impl LuaScript {
+    fn new(content: String) -> LuaScript {
+        let lua = Lua::new();
+        LuaScript {
+            lua: Box::new(lua),
+            content,
+        }
+    }
+    fn setup_env(&mut self) {
+        let events = self.lua.create_table().unwrap();
+        {
+            let on_server_callbacks = self.lua.create_table().unwrap();
+            self.lua
+                .globals()
+                .set("__on_server_callbacks", on_server_callbacks)
+                .unwrap();
+            let on_server = self
+                .lua
+                .create_function(|lua, callback: LuaFunction| {
+                    lua.load(chunk! {
+                        __on_server_callbacks[#__on_server_callbacks+1] = $callback
+                    })
+                    .exec()
+                    .unwrap();
+                    Ok(())
+                })
+                .unwrap();
+            events.set("on_server", on_server).unwrap();
+        }
+        {
+            let on_client_callbacks = self.lua.create_table().unwrap();
+            self.lua
+                .globals()
+                .set("__on_client_callbacks", on_client_callbacks)
+                .unwrap();
+            let on_client = self
+                .lua
+                .create_function(|lua, callback: LuaFunction| {
+                    lua.load(chunk! {
+                        __on_client_callbacks[#__on_client_callbacks+1] = $callback
+                    })
+                    .exec()
+                    .unwrap();
+                    Ok(())
+                })
+                .unwrap();
+            events.set("on_client", on_client).unwrap();
+        }
+        {
+            let on_message_callbacks = self.lua.create_table().unwrap();
+            self.lua
+                .globals()
+                .set("__on_message_callbacks", on_message_callbacks)
+                .unwrap();
+            let on_message = self
+                .lua
+                .create_function(|lua, callback: LuaFunction| {
+                    lua.load(chunk! {
+                        __on_message_callbacks[#__on_message_callbacks+1] = $callback
+                    })
+                    .exec()
+                    .unwrap();
+                    Ok(())
+                })
+                .unwrap();
+            events.set("on_message", on_message).unwrap();
+        }
+        self.lua.globals().set("events", events).unwrap();
+    }
+    pub fn start(&mut self) {
+        if let Err(e) = self.lua.load(&self.content).exec() {
+            error!("Lua Environment: {e}");
+        };
+    }
+}
+
+pub struct LuaLoader {
+    pub scripts: Vec<LuaScript>,
+}
+impl LuaLoader {
+    pub fn new() -> Self {
+        LuaLoader { scripts: vec![] }
+    }
+    pub fn content(&mut self, content: &str) {
+        //let content = std::fs::read_to_string(file_name).unwrap();
+        let mut script = LuaScript::new(content.to_owned());
+        script.setup_env();
+        self.scripts.push(script);
     }
 }
